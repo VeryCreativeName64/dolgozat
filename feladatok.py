7a61e10cd7d7fb77ae7fb9ed52f183652b81b6cc
Marek Bendegúz Kristóf (Szf_N_2024_2026) <marekbendeguz@gmail.com>
2024-11-07 09:18:08 +0100

    5. feladat kész!

--- feladatok.py
+++ feladatok.py
@@ -30,6 +30,33 @@ def feladat_4():
     if(nevek=="@"):
         print(f"A felhasználó {db} nevet adott meg.")
         
+def feladat_5():
+    felhasznalo_tippje=input("Válassz kő, papír, vagy olló közül: ").lower()
+    gep_tippje=int(random.random()*3+1)
+    while(felhasznalo_tippje!="kő" and felhasznalo_tippje!="papír" and felhasznalo_tippje!="olló"):
+        felhasznalo_tippje=input("Mondom válassz kő, papír, vagy olló közül: ").lower()
+    else:
+        if(felhasznalo_tippje=="kő" and gep_tippje==1):
+            print("Döntetlen, ugyanúgy kőre gondoltunk!")
+        elif(felhasznalo_tippje=="papír" and gep_tippje==2):
+            print("Döntetlen, ugyanúgy papírra gondoltunk!")
+        elif(felhasznalo_tippje=="olló" and gep_tippje==3):
+            print("Döntetlen, ugyanúgy ollóra gondoltunk!")
+        elif(felhasznalo_tippje=="kő" and gep_tippje==2):
+            print("Te kőre, én papírra gondoltam, így nyertem!")
+        elif(felhasznalo_tippje=="kő" and gep_tippje==3):
+            print("Te kőre, én ollóra gondoltam, így nyertél!")
+        elif(felhasznalo_tippje=="papír" and gep_tippje==1):
+            print("Te papírra, én kőre gondoltam, így nyertél!")
+        elif(felhasznalo_tippje=="papír" and gep_tippje==3):
+            print("Te papírra, én ollóra gondoltam, így nyertem!")
+        elif(felhasznalo_tippje=="olló" and gep_tippje==1):
+            print("Te ollóra, én kőre gondoltam, így nyertem!")
+        elif(felhasznalo_tippje=="olló" and gep_tippje==2):
+            print("Te ollóra, én papírra gondoltam, így nyertél!")
+
+
+    
 
 
 
--- main.py
+++ main.py
@@ -2,4 +2,5 @@ import feladatok
 
 #feladatok.feladat_1()
 #feladatok.feladat_2()
-feladatok.feladat_4()
+#feladatok.feladat_4()
+feladatok.feladat_5()
